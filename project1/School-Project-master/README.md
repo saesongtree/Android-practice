c9399cb14be5a06e06d469032a1ed373897ec263
saesongtree <162665119+saesongtree@users.noreply.github.com>
2024-04-03 03:19:41 +0900

    Add files via upload

---
+++ project1/School-Project-master/README.md
@@ -0,0 +1,2 @@
+# School-Project
+# 졸업 프로젝트
---
+++ project1/School-Project-master/app/src/main/java/com/example/project1/Recommandhospital.java
@@ -0,0 +1,38 @@
+package com.example.project1;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class Recommandhospital extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_recommandhospital);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        ImageView arrowIcon = findViewById(R.id.arrowIcon);
+        arrowIcon.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Recommandhospital.this, CheckActivity.class);
+                startActivity(intent);
+            }
+        });
+
+
+    }
+}
